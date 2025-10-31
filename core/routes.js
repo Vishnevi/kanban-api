c953aa9443af1c18c32bb8c8af0e5fc4d40aa352
Vishnevi <wewevilka@gmail.com>
2025-10-31 04:52:56 +0300

    start

--- core/routes.js
+++ core/routes.js
@@ -1,8 +1,9 @@
 import cardRouter from "../cards/cardRoutes.js";
-
+import homeRouter from "../home/homeRoutes.js";
 
 // роуты
 export default function routes(app){
     app.use('/cards', cardRouter);
+    app.use('/', homeRouter);
 }
 
