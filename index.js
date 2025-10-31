f2dda7c6a952a33c7304106193fdcd19cd5ad069
Vishnevi <wewevilka@gmail.com>
2025-10-31 04:29:12 +0300

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import logger from "./core/logger.js";
 import routes from "./core/routes.js";
 import dbConnect from "./core/db.js";
 const app = express();
-const PORT = 3000;
+const PORT =  process.env.PORT || 5000;
 
 dbConnect();
 logger(app);
