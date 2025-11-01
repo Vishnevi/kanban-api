5a3aa615a2a507df73274d5b1a16c55863a0be34
Vishnevi <wewevilka@gmail.com>
2025-11-01 04:35:16 +0300

    database connection

--- core/db.js
+++ core/db.js
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
+const connectionString = 'mongodb+srv://robertkhvaleev3_db_user:gErtI200676@cluster0.7kx5kih.mongodb.net/?appName=Cluster0';
+
+
 function dbConnect() {
-    mongoose.connect('mongodb://localhost/kanban')
+    mongoose.connect(connectionString)
         .then(() => console.log('✅ MongoDB connected'))
         .catch(err => console.error('❌ Connection error:', err));
 }
