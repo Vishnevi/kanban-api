daa9731c732e6711234362da7a82cc7681f7e596
Vishnevi <wewevilka@gmail.com>
2025-11-01 04:54:04 +0300

    test commit

--- cards/cardGetAll.js
+++ cards/cardGetAll.js
@@ -1,12 +1,13 @@
 import Card from './Model.js'
 
 export default function cardGetAll(req, res) { // роут GET localhost:3000/cards
-    Card.find()
-        .exec()
-        .then(result => {
-            res.status(200).json(result);
-        }).catch(err => {
-            console.log(err);
-            res.status(400).json('Cards get all failed');
-    });
+    //Card.find()
+    //    .exec()
+    //    .then(result => {
+    //        res.status(200).json(result);
+    //    }).catch(err => {
+    //        console.log(err);
+    //        res.status(400).json('Cards get all failed');
+    res.send('Card GetAll');
+    //});
 }
